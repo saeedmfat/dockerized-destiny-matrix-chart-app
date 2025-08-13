f24306ca620dc2986e1a15484d63ed3a63a98e17
saeedmfat <saeedmarefat7@gmail.com>
2025-08-14 03:17:16 +0330

    define lib in  backend/app/database.py

---
+++ backend/app/database.py
@@ -0,0 +1,4 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
